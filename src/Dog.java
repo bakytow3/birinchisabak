bd521d0fc895f5bd1d61f6b0dab010f0c4073f55
bakytow3 <bakytovnurislam38@gmail.com>
2022-08-06 20:23:58 +0600

    3Commit

---
+++ src/Dog.java
@@ -0,0 +1,24 @@
+public class Dog {
+    private String name;
+    private int age;
+    private int baasy;
+    void setName(String tt){
+        name=tt;
+    }
+    void setAge(int ff){
+        age=ff;
+
+    }
+    void setBaasy(int gg){
+        baasy=gg;
+    }
+    String getName(){
+        return name;
+    }
+    int getAge(){
+        return age;
+    }
+    int getBaasy(){
+        return baasy;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,99 @@
 
+
 public class  Main {
+
+
     public static void main(String[] args) {
 
+Balyk balyk=new Balyk();
+balyk.setName("Akula");
+String cc= balyk.getName();
+        System.out.println(cc);
+        balyk.setAge(6);
+        int kk= balyk.getAge();
+        System.out.println(kk);
+        balyk.setBaasy(15500);
+        int gg= balyk.getBaasy();
+        System.out.println(gg);
+
+Popugay popugay=new Popugay();
+popugay.setName("Gosha");
+String tt = popugay.getName();
+        System.out.println(tt);
+        popugay.setAge(4);
+        int agee= popugay.getAge();
+        System.out.println(agee);
+        popugay.setBaasy(120000);
+        int baaasy= popugay.getBaasy();
+        System.out.println(baaasy);
+
+Myshyk myshyk=new Myshyk();
+myshyk.setName("Doriyan");
+String aty=myshyk.getName();
+        System.out.println(aty);
+        myshyk.setAge(3);
+        int aa=myshyk.getAge();
+        System.out.println(aa);
+        myshyk.setBaasy(130000);
+        int bb=myshyk.getBaasy();
+        System.out.println(bb);
+
+Dog dog=new Dog();
+dog.setName("Bobik");
+String vv= dog.getName();
+        System.out.println(vv);
+        dog.setAge(2);
+        int mm= dog.getAge();
+        System.out.println(mm);
+        dog.setBaasy(14000);
+        int nn= dog.getBaasy();
+        System.out.println(nn);
         }
-
-
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
