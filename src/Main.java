ca2234595beb5ace3dfc331d1357814b1677c0f2
bakytow3 <bakytovnurislam38@gmail.com>
2022-08-12 14:36:47 +0600

    14 tapshyrma

--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,42 @@ public class  Main {
 
 
     public static void main(String[] args) {
+        Person person = new Person("Michael Jordan", "former professional basketball player");
+        Programmer programmer = new Programmer("Bill Gates", "founder of Microsoft", "Microsoft");
+        Dancer dancer = new Dancer("Atai Omurzakov", "famous dancer", "Atai and group");
+        Singer singer = new Singer("Celine Dion", "She has won five Grammy Awards", "Dion-Angélil");
+        System.out.println(person);
+        person.learn();
+        person.walk();
+        person.eat();
+        System.out.println(programmer);
+        programmer.learn();
+        programmer.walk();
+        programmer.eat();
+        programmer.coding();
+        System.out.println(dancer);
+        dancer.learn();
+        dancer.walk();
+        dancer.eat();
+        dancer.dancing();
 
-Balyk balyk=new Balyk();
-balyk.setName("Akula");
-String cc= balyk.getName();
-        System.out.println(cc);
-        balyk.setAge(6);
-        int kk= balyk.getAge();
-        System.out.println(kk);
-        balyk.setBaasy(15500);
-        int gg= balyk.getBaasy();
-        System.out.println(gg);
+        System.out.println(singer);
 
-Popugay popugay=new Popugay();
-popugay.setName("Gosha");
-String tt = popugay.getName();
-        System.out.println(tt);
-        popugay.setAge(4);
-        int agee= popugay.getAge();
-        System.out.println(agee);
-        popugay.setBaasy(120000);
-        int baaasy= popugay.getBaasy();
-        System.out.println(baaasy);
+        singer.learn();
+        singer.walk();
+        singer.eat();
+        singer.singing();
+        singer.playGitar();
 
-Myshyk myshyk=new Myshyk();
-myshyk.setName("Doriyan");
-String aty=myshyk.getName();
-        System.out.println(aty);
-        myshyk.setAge(3);
-        int aa=myshyk.getAge();
-        System.out.println(aa);
-        myshyk.setBaasy(130000);
-        int bb=myshyk.getBaasy();
-        System.out.println(bb);
-
-Dog dog=new Dog();
-dog.setName("Bobik");
-String vv= dog.getName();
-        System.out.println(vv);
-        dog.setAge(2);
-        int mm= dog.getAge();
-        System.out.println(mm);
-        dog.setBaasy(14000);
-        int nn= dog.getBaasy();
-        System.out.println(nn);
-        }
     }
+}
+
+
+
+
+
+
+
 
 
 
