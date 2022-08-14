7b72cc61bdfa5af4cbd56d3b6af61c5c1d4d6971
bakytow3 <bakytovnurislam38@gmail.com>
2022-08-14 21:22:51 +0600

    15 tapshyrma

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,13 @@
-
+import java.time.LocalDate;
 
 public class  Main {
-
-
     public static void main(String[] args) {
-        Person person = new Person("Michael Jordan", "former professional basketball player");
-        Programmer programmer = new Programmer("Bill Gates", "founder of Microsoft", "Microsoft");
-        Dancer dancer = new Dancer("Atai Omurzakov", "famous dancer", "Atai and group");
-        Singer singer = new Singer("Celine Dion.", "She has won five Grammy Awards", "Dion-Angélil");
-        System.out.println(person);
-        person.learn();
-        person.walk();
-        person.eat();
-        System.out.println(programmer);
-        programmer.learn();
-        programmer.walk();
-        programmer.eat();
-        programmer.coding();
-        System.out.println(dancer);
-        dancer.learn();
-        dancer.walk();
-        dancer.eat();
-        dancer.dancing();
-
-        System.out.println(singer);
-
-        singer.learn();
-        singer.walk();
-        singer.eat();
-        singer.singing();
-        singer.playGitar();
-
+        Circle circle=new Circle();
+        circle.area(10);
+        circle.circumference(20.4);
     }
 }
+
 
 
 
