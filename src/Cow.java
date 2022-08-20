e24cddc4cde55c9947745cd07adbca0c79a35f05
bakytow3 <bakytovnurislam38@gmail.com>
2022-08-20 18:51:09 +0600

    17.2tapshyrma

--- src/Cow.java
+++
@@ -1,5 +0,0 @@
-public class Cow extends Animal{
-    public Cow(int weight, int age, String gender, String nickName) {
-        super(weight, age, gender, nickName);
-    }
-}
--- src/Sheep.java
+++
@@ -1,6 +0,0 @@
-public class Sheep extends Animal {
-
-    public Sheep(int weight, int age, String gender, String nickName) {
-        super(weight, age, gender, nickName);
-    }
-}
