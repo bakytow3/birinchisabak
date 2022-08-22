e2cd849dad09baac1f7315ff465ce1f5de36e886
bakytow3 <bakytovnurislam38@gmail.com>
2022-08-22 13:18:03 +0600

    18tapshyrma

---
+++ src/Kvartira.java
@@ -0,0 +1,15 @@
+public class Kvartira implements Hint{
+
+    @Override
+    public void KomUslugaKvartira() {
+        System.out.println("KomUslugaga 7000 Toloyty");
+    }
+
+    @Override
+    public void Arenda()
+    {
+String[]Uybuloo1=new String[1];
+Uybuloo1[0]="Kvartirada 3 kishi";
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-Shape kvadrat=new Kvadrat();
-kvadrat.getPerimeter();
-Shape krugliy=new Krugliy();
-krugliy.getPerimeter();
-Shape priyamoUgolny=new PriyamoUgolny();
-priyamoUgolny.getPerimeter();
-Shape triUgolnik=new TriUgolnik();
-triUgolnik.getPerimeter();
-Shape romb =new Romb();
-romb.getPerimeter();
+        Hint kvartira=new Kvartira();
+        kvartira.KomUslugaKvartira();
+        String[]Uybuloo1=new String[1];
+        Uybuloo1[0]="Kvartirada 3 kishi";
+        System.out.println(Uybuloo1[0]);
+        Obshejitiya obshejitiya=new Obshejitiya();
+        obshejitiya.Arenda();
+        String[]Uybuloo2=new String[1];
+        Uybuloo2[0]="Obshejitiyada 4 kishi";
+        System.out.println(Uybuloo2[0]);
+        Hint gostinisa=new Gostinisa();
+        gostinisa.Arenda();
+        String[]Uybuloo3=new String[1];
+        Uybuloo3[0]="Gostinisada 2 kishi";
+        System.out.println(Uybuloo3[0]);
     }
 }
